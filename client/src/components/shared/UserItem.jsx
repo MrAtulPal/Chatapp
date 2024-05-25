0fa5f15445ffb5690cf29e8ae4c6cdb5ea92c27a
Atul Pal <mr.atulofficial.in@gmail.com>
2024-05-25 18:27:24 +0530

    new group UI done

--- client/src/components/shared/UserItem.jsx
+++ client/src/components/shared/UserItem.jsx
@@ -1,8 +1,8 @@
-import { Add as AddIcon } from "@mui/icons-material";
+import { Add as AddIcon, Remove as RemoveIcon } from "@mui/icons-material";
 import { Avatar, IconButton, ListItem, Stack, Typography } from "@mui/material";
 import React, { memo } from "react";
 
-const UserItem = ({ user, handler, handlerIsLoading }) => {
+const UserItem = ({ user, handler, handlerIsLoading, isAdded= false}) => {
   const { name, _id, avatar } = user;
   return (
     <ListItem>
@@ -23,24 +23,24 @@ const UserItem = ({ user, handler, handlerIsLoading }) => {
             display: "-webkit-box",
             WebkitLineClamp: 1,
             WebkitBoxOrient: "vertical",
-            width: '100%'
+            width: "100%",
           }}
         >
           {name}
         </Typography>
         <IconButton
           sx={{
-            bgcolor: "primary.main",
+            bgcolor: isAdded ? "error.main" : "primary.main",
             color: "white",
-            "&.hover": {
-              bgcolor: "primary.dark",
+            "&:hover": {
+              bgcolor: isAdded ? "error.dark" : "primary.dark",
             },
           }}
           size="small"
           onClick={() => handler(_id)}
           disabled={handlerIsLoading}
         >
-          <AddIcon />
+          {isAdded ? <RemoveIcon /> : <AddIcon />}
         </IconButton>
       </Stack>
     </ListItem>
--- client/src/components/specific/NewGroup.jsx
+++ client/src/components/specific/NewGroup.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import { useInputValidation } from "6pp";
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import { sampleUser } from "../../constants/sampleChats";
+import UserItem from "../shared/UserItem";
 
 const NewGroup = () => {
-  return (
-    <div>NewGroup</div>
-  )
-}
+  const [members, setMembers] = React.useState(sampleUser);
+  const [selectedMembers, setselectedMembers] = React.useState([]);
+  const isLoadingSendFriendRequest = false;
 
-export default NewGroup
+  const groupName = useInputValidation("");
+  const selectMemberHandler = (id) => {
+    setselectedMembers((prev) =>
+      prev.includes(id) ? prev.filter((i) => i !== id) : [...prev, id]
+    );
+  };
+
+  const closeHandler = () => {};
+
+  return (
+    <Dialog open onClose={closeHandler}>
+      <Stack p={{ xs: "1rem", sm: "3rem" }} width={"25rem"} spacing={"2rem"}>
+        <DialogTitle variant="h4" textAlign={"center"}>
+          New Group
+        </DialogTitle>
+
+        <TextField
+          label="Group Name"
+          value={groupName.value}
+          onChange={groupName.changeHandler}
+        />
+        <Typography></Typography>
+
+        <Stack>
+          {members.map((user) => (
+            <UserItem
+              user={user}
+              key={user._id}
+              handler={selectMemberHandler}
+              handlerIsLoading={isLoadingSendFriendRequest}
+              isAdded={selectedMembers.includes(user._id)}
+            />
+          ))}
+        </Stack>
+        <Stack
+          direction={"row"}
+          width={"100%"}
+          justifyContent={"space-between"}
+        >
+          <Button variant="outlined" color="error">
+            Cancel
+          </Button>
+          <Button variant={"contained"}> Create</Button>
+        </Stack>
+      </Stack>
+    </Dialog>
+  );
+};
+
+export default NewGroup;
